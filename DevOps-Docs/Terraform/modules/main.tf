c24ffee4e30420707839abe42851e48d49d1c2d5
Sridhar Akella <macbookpro@MacBooks-MBP.lan>
2025-10-07 20:58:20 -0400

    check-in

--- DevOps-Docs/Terraform/modules/main.tf
+++ DevOps-Docs/Terraform/modules/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   
          access_key = "AKIAXEFUNJMJJKSUL363"
-                       secret_key = "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"
+                       secret_key = ""
 }
 
 resource "aws_instance" "test" {
